bcd6f4b87d0594707bd7d48a922d7a8f2e678705
jokerstylekz-sudo <jokerstyle.kz@gmail.com>
2026-01-15 12:04:18 +0100

    PlayerClass started

---
+++ player.py
@@ -0,0 +1,13 @@
+from constants import PLAYER_RADIUS
+class Player:
+    def __init__(self, x, y, radius):
+    super().__init__()
+    rotation=0
+# in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
