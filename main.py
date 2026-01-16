b0f8a608f7e6e7956be0f66966dd3561e3b73b86
jokerstylekz-sudo <jokerstyle.kz@gmail.com>
2026-01-16 13:30:37 +0100

    Asteroids are spawning

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
     clock=pygame.time.Clock()
     dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,9 +33,9 @@ def main():
         screen.fill("black")
         framerate=clock.tick(60)
         dt= framerate/1000
-        player.update(dt)
-        player.draw(screen)
-
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         
         
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 from circleshape import CircleShape
 
@@ -8,6 +8,11 @@ class Player(CircleShape):
         self.rotation=0
     def rotate(self, dt):
         self.rotation+=PLAYER_TURN_SPEED *dt
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -15,6 +20,10 @@ class Player(CircleShape):
             self.rotate(-dt) #for point 4 like this?
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 # in the Player class
